9022df84932a1e80516ea563f56b7d363bd736bb
David Noreña Correa <dnorena@celsia.com>
2021-12-28 10:22:21 -0500

    retos internet navidad

---
+++ src/adventofcode/sonar_sweep/dia1_reto2.py
@@ -0,0 +1,36 @@
+import io
+import string
+
+alphabet = list(string.ascii_lowercase)
+checked_measurements = []
+last_read = 0
+increased = 0
+first_record = 0
+with open("./medidas.txt", "r") as measurements:
+    print(measurements)
+    """
+    for letter in alphabet:
+        measure = int(m)
+        if(first_record == 0):
+            checked_measurements.append(f"{measure} (N/A no previous measurement)")
+            print(f"{measurements} (N/A no previous measurement)")
+            first_record += 1
+        else:
+            if(measure > last_read):
+                checked_measurements.append(f"{measure} (increased)")
+                print(f"{measure} (increased)")
+                increased += 1
+            elif(measure < last_read):
+                checked_measurements.append(f"{measure} (decreased)")
+                print(f"{measure} (decreased)")
+            else:
+                checked_measurements.append(f"{measure} (stay)")
+                print(f"{measure} (stay)")
+        last_read = measure
+
+with open("./medidas_checked.txt", "w") as mc:
+    for line in checked_measurements:
+        mc.write(f"{line}\n")
+
+print(f"el número de incrementos es de..: {increased}")
+"""
